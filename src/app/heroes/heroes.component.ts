27384019f756ca5a77eba7ba152946941058bcda
Daniel Glazer <danielglazer123@gmail.com>
2020-06-09 08:40:30 +0300

    Add In-app Navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,10 +11,8 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
-  constructor(private heroService: HeroService,
-    private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void { this.getHeroes(); }
 
@@ -23,10 +21,5 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.
-      add(`HeroService: Selected hero id=${hero.id}`);
-  }
 
 }
